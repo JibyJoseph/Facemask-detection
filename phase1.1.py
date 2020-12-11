9d12c933b4b56a9dfdebd37e03296d6eeb8de6c4
Kiran_Joy <51417521+UnkeptHat88@users.noreply.github.com>
2020-12-11 10:21:31 +0530

    phase1.2.py

--- phase1.1.py
+++ phase1.1.py
@@ -1,188 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-import cv2
-import numpy as np
-import os
-from random import shuffle
-from tqdm import tqdm
-
-TRAIN_DIR = "D:/Project/4th year/Project files/Project/train"
-TEST_DIR = "D:/Project/4th year/Project files/Project/test"
-IMG_SIZE = 50
-LR = 1e-3
-
-MODEL_NAME = "dogsvscats--{}--{}.model".format(LR, '2conv-basicVS')
-
-def label_img(img):
-    word_label = img.split('.')[0]
-    if word_label == 'cat' : return [1,0]
-    elif word_label == 'dog' : return [0,1]
-
-def create_train_data():
-    training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        label = label_img(img)
-        path = os.path.join(TRAIN_DIR,img)
-        img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-        training_data.append([np.array(img),np.array(label)])
-    shuffle(training_data)
-    #np.save('train_data.npy',training_data,allow_pickle=True)
-    return training_data
-
-def process_test_data():
-    testing_data = []
-    for img in tqdm(os.listdir(TEST_DIR)):
-        label = label_img(img)
-        path = os.path.join(TEST_DIR,img)
-        img_num = img.split('.')[0]
-        img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-        testing_data.append([np.array(img),np.array(img_num)])
-    #np.save('test_data.npy',testing_data)
-    return testing_data
-
-train_data = create_train_data()
-
-
-# In[4]:
-
-
-import cv2
-import numpy as np
-import os
-from random import shuffle
-from tqdm import tqdm
-
-TRAIN_DIR = "D:/Project/4th year/Project files/Project/train"
-TEST_DIR = "D:/Project/4th year/Project files/Project/test"
-IMG_SIZE = 50
-LR = 1e-3
-
-MODEL_NAME = "dogsvscats--{}--{}.model".format(LR, '2conv-basicVS')
-
-def label_img(img):
-    word_label = img.split('.')[0]
-    if word_label == 'cat' : return [1,0]
-    elif word_label == 'dog' : return [0,1]
-
-def create_train_data():
-    training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        label = label_img(img)
-        path = os.path.join(TRAIN_DIR,img)
-        img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-        training_data.append([np.array(img),np.array(label)])
-    shuffle(training_data)
-    #np.save('train_data.npy',training_data,allow_pickle=True)
-    return training_data
-
-def process_test_data():
-    testing_data = []
-    for img in tqdm(os.listdir(TEST_DIR)):
-        label = label_img(img)
-        path = os.path.join(TEST_DIR,img)
-        img_num = img.split('.')[0]
-        img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-        testing_data.append([np.array(img),np.array(img_num)])
-    #np.save('test_data.npy',testing_data)
-    return testing_data
-
-train_data = create_train_data()
-
-
-# In[6]:
-
-
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-
-import tensorflow as tf
-tf.reset_default_graph()
-
-convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
-
-convnet = conv_2d(convnet, 32, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 64, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 32, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 64, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 32, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 64, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 64, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.5)
-
-convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-
-model = tflearn.DNN(convnet, tensorboard_dir='log')
-
-
-train = train_data[:-500]
-test = train_data[-500:]
-
-X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-Y = [i[1] for i in train]
-
-test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-test_y = [i[1] for i in test]
-
-model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}), 
-    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
-
-
-# In[7]:
-
-
-model.fit({'input': X}, {'targets': Y}, n_epoch=5, validation_set=({'input': test_x}, {'targets': test_y}), 
-    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
-
-
-# In[8]:
-
-
-inputTest = 'D:/Project/4th year/Project files/Project/img_1317.jpg'
-image = cv2.resize(cv2.imread(inputTest,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-model.predict(np.array(image).reshape(-1,IMG_SIZE,IMG_SIZE,1))
-
-
-# In[11]:
-
-
-inputTest = 'D:/Project/4th year/Project files/Project/1111.jpg'
-image = cv2.resize(cv2.imread(inputTest,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-model.predict(np.array(image).reshape(-1,IMG_SIZE,IMG_SIZE,1))
-
-
-# In[10]:
-
-
-inputTest = 'D:/Project/4th year/Project files/Project/1111.jpg'
-image = cv2.resize(cv2.imread(inputTest,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
-model.predict(np.array(image).reshape(-1,IMG_SIZE,IMG_SIZE,1))
-
-
-# In[49]:
-
-
 import cv2
 import numpy as np
 import os
@@ -202,41 +17,41 @@ MODEL_NAME = "faceMaskBinary--{}--{}.model".format(LR, '6conv-JPY1')
 
 # In[26]:
 
+def Extract_Unmasked_data():
+    f_dir = 'D:/Project/4th year/Project files/Datasets/archive/AFDB_face_dataset/AFDB_face_dataset'
+    d_dir = 'D:/Project/4th year/Project files/Project/facemask/face'
+    x=0
+    for folder in tqdm(os.listdir(f_dir)):
+        path = os.path.join(f_dir,folder)
+        y=0
+        for img in os.listdir(path):
+            if y>=10: # To make neural net more robust we took only 10 pictures from each person
+                break
+            if x>=4000: # extracted only 4000 unmasked imgaes because only 1400 masked images exits
+                break
+            fpath = os.path.join(path,img)
+            dpath = os.path.join(d_dir,str(x)+'.jpg')
+            copyfile(fpath, dpath)
+            x+=1
+            y+=1
 
-train_face_dir = 'D:/Project/4th year/Project files/Datasets/archive/AFDB_face_dataset/AFDB_face_dataset'
-train_mask_dir = 'D:/Project/4th year/Project files/Project/facemask/face'
-x=0
-for folder in tqdm(os.listdir(train_face_dir)):
-    path = os.path.join(train_face_dir,folder)
-    y=0
-    for img in os.listdir(path):
-        if y>=10:
-            break
-        if x>=4000:
-            break
-        fpath = os.path.join(path,img)
-        dpath = os.path.join(train_mask_dir,str(x)+'.jpg')
-        copyfile(fpath, dpath)
-        x+=1
-        y+=1
-
-
-# In[27]:
-
-
-train_face_dir = 'D:/Project/4th year/Project files/Datasets/archive/AFDB_masked_face_dataset/AFDB_masked_face_dataset'
-train_mask_dir = 'D:/Project/4th year/Project files/Project/facemask/mask'
-x=0
-for folder in tqdm(os.listdir(train_face_dir)):
-    path = os.path.join(train_face_dir,folder)
-    for img in os.listdir(path):
-        fpath = os.path.join(path,img)
-        dpath = os.path.join(train_mask_dir,str(x)+'.jpg')
-        copyfile(fpath, dpath)
-        x+=1
+def Extract_Masked_data():
+    from_dir = 'D:/Project/4th year/Project files/Datasets/archive/AFDB_masked_face_dataset/AFDB_masked_face_dataset'
+    to_dir = 'D:/Project/4th year/Project files/Project/facemask/mask'
+    x=0
+    for folder in tqdm(os.listdir(from_dir)):
+        path = os.path.join(from_dir,folder)
+        for img in os.listdir(path):
+            fpath = os.path.join(path,img)
+            dpath = os.path.join(to_dir,str(x)+'.jpg')
+            copyfile(fpath, dpath)
+            x+=1
 
 
-# In[98]:
+            
+Extract_Unmasked_data()
+Extract_Masked_data()
+
 
 
 data_face = 'D:/Project/4th year/Project files/Project/facemask/face'
